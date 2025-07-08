018d17f8b5939c67d36bf53f95d21595656ed10a
Lablazz <mamiron012@gmail.com>
2025-07-08 15:29:00 +0700

    Setup New Project

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyProject.Models;
+
+namespace MyProject.Data;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+    public DbSet<User> Users => Set<User>();
+}
